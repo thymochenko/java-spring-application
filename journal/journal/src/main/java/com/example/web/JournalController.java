4b5717e2ae73673b56b63baa72bda7651d304d1c
thymochenko <henkosato5@gmail.com>
2016-09-09 16:36:53 -0300

    finalizado cap1

--- journal/journal/src/main/java/com/example/web/JournalController.java
+++ journal/journal/src/main/java/com/example/web/JournalController.java
@@ -1,12 +1,22 @@
 package com.example.web;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.repository.JournalRepository;
+import com.example.domain.Journal;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class JournalController {
+
+  @RequestMapping(value="/journal", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+  public @ResponseBody List<Journal> getJournal(){
+    return repo.findAll();
+  }
 
   @Autowired
   JournalRepository repo;
